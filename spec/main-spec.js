212c676bd4a4c4aed28887da9a66069875544b7d
tianmali <408267415@qq.com>
2014-11-16 18:51:30 +0800

    test two different items without - and without promotion

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -11,4 +11,18 @@ describe("pos-tdd-v2",function(){
      expect(console.log).toHaveBeenCalledWith(expectedResult);
 
   });
+  it("test two different items without '-' and without promotion",function(){
+    spyOn(console, 'log');
+    var inputStr = ["ITEM000003",
+                    "ITEM000004"],
+        expectedResult =  "***<没钱赚商店>购物清单***\n" +
+                          "名称：荔枝，数量：1斤，单价：15.00(元)，小计：15.00(元)\n"+
+                          "名称：电池，数量：1个，单价：2.00(元)，小计：2.00(元)\n"+
+                          "----------------------\n" +
+                          "总计：17.00(元)\n" +
+                          "**********************";
+     printInventory(inputStr);
+     expect(console.log).toHaveBeenCalledWith(expectedResult);
+
+  });
 });
