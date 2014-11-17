c5354e08d1404712b1d91622ca39366dcc2952e0
tianmali <408267415@qq.com>
2014-11-17 21:37:37 +0800

    did a little refactor

---
+++ src/Handle/itemHandle.js
@@ -0,0 +1,28 @@
+function itemHandle(){
+  this.item = function getItemWithBarCode(barCode){
+    var allItems = loadAllItems();
+    var item = '';
+    for(var i in allItems){
+      if(barCode.indexOf(allItems[i].barcode) === 0){
+        item = allItems[i];
+      }
+    }
+    return item;
+  };
+
+  this.withoutSameItem = function (input){
+    var index = 0;
+    var array = [];
+    var oneItem = input[0];
+    array.push(oneItem);
+    while(index<input.length){
+       var simple = input[index];
+       if(input[index].indexOf(oneItem) == -1){
+        array.push(input[index]);
+         oneItem = simple;
+       }
+      index++;
+    }
+    return array;
+  };
+}
--- src/main.js
+++ src/main.js
@@ -1,30 +1,26 @@
 
-//名称：荔枝，数量：1斤，单价：15.00(元)，小计：15.00(元) ---------------------- 总计：15.00(元)
+var itemHand = new itemHandle();
 function printInventory(inputs) {
   var start='***<没钱赚商店>购物清单***\n';
   var end = '**********************';
   var segmentLine = '----------------------\n';
-  var middle = '';
+  var allString = '';
+  var simpleInput = itemHand.withoutSameItem(inputs);
+  var subtotalString ='';
+  var totalString ='';
   var total = 0;
-  var allItems = loadAllItems();
   for(var j in inputs){
     var input = inputs[j];
-    var item;
-    for(var i in allItems){
-      if(allItems[i].barcode === input){
-        item = allItems[i];
-        middle +=  '名称：'+ item.name +
+    var item = itemHand.item(input);
+    subtotalString +=  '名称：'+ item.name +
                    '，数量：1' + item.unit +
                    '，单价：' + item.price.toFixed(2) + '(元)，小计：' +
                    item.price.toFixed(2) + '(元)\n';
         total += item.price;
-      }
+        totalString = '总计：' + total.toFixed(2) + '(元)\n';
 
-    }
   }
-
-  var totalString = '总计：' + total.toFixed(2) + '(元)\n';
-  var output = start + middle+segmentLine + totalString +end;
+  var output = start + subtotalString + segmentLine + totalString +end;
   console.log(output);
 
 }
