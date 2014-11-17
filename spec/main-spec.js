025edec3083dab19003fcef30575e87faca2973b
tianmali <408267415@qq.com>
2014-11-17 20:16:36 +0800

    test same items without '-' and without promotion

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -25,4 +25,19 @@ describe("pos-tdd-v2",function(){
      expect(console.log).toHaveBeenCalledWith(expectedResult);
 
   });
+  it("test same items without '-' and without promotion",function(){
+    spyOn(console, 'log');
+    var inputStr = ["ITEM000003",
+                    "ITEM000003",
+                    "ITEM000004"],
+        expectedResult =  "***<没钱赚商店>购物清单***\n" +
+                          "名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n"+
+                          "名称：电池，数量：1个，单价：2.00(元)，小计：2.00(元)\n"+
+                          "----------------------\n" +
+                          "总计：32.00(元)\n" +
+                          "**********************";
+     printInventory(inputStr);
+     expect(console.log).toHaveBeenCalledWith(expectedResult);
+
+  });
 });
